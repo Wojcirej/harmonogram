ee4a0073d50dbae1de7323d1def2f79c935056f6
Wojcirej <wojcirej@gmail.com>
2017-06-18 23:09:44 +0200

    Zmiany w menu + przykładowe plany zajęć.

--- js/ang.js
+++ js/ang.js
@@ -3,7 +3,7 @@ angular.module('harmonogram', ['ngRoute'])
   $scope.y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   $scope.x = [0, 1, 2, 3, 4, 5, 6, 7];
   $scope.instytut = 0;
-  $scope.zaklad = 0;
+  $scope.zaklad;
   $scope.instytuty = [
     {index:0,title:' '},
     {index:1,title:'Instytut Gospodarki i Polityki Społecznej'},
@@ -30,11 +30,31 @@ angular.module('harmonogram', ['ngRoute'])
     {z:15,i:4,title:'Zakład Mechaniki i Budowy Maszyn'},
     {z:16,i:4,title:'Zakład Zarządzania'},
   ];
+  $scope.plany = [
+    {z:13,x:0,y:0,content:'Fizyka - wykład'},
+    {z:13,x:0,y:1,content:'Fizyka - wykład'},
+    {z:13,x:0,y:9,content:'Algebra liniowa - ćwiczenia'},
+    {z:13,x:0,y:10,content:'Algebra liniowa - ćwiczenia'},
+    {z:13,x:2,y:2,content:'Język angielski - ćwiczenia'},
+    {z:13,x:2,y:3,content:'Język angielski - ćwiczenia'},
+    {z:13,x:2,y:4,content:'Język angielski - ćwiczenia'},
+    {z:3,x:2,y:4,content:'Catering żywności - wykład'},
+    {z:3,x:2,y:5,content:'Catering żywności - wykład'},
+    {z:3,x:4,y:10,content:'Komputerowe wspomagania systemów zarządzania jakości - wykład'},
+    {z:3,x:4,y:11,content:'Komputerowe wspomagania systemów zarządzania jakości - wykład'},
+    {z:3,x:4,y:12,content:'Komputerowe wspomagania systemów zarządzania jakości - wykład'},
+  ];
   $scope.wybierzInstytut = function(index) {
     $scope.instytut = index;
   }
   $scope.wybierzZaklad = function(index) {
     $scope.zaklad = index;
+  }
+  $scope.pobierzInstytut = function() {
+    return $scope.instytut;
+  }
+  $scope.pobierzZaklad = function() {
+    return $scope.zaklad;
   }
   $scope.reset = function() {
     $scope.zaklad = 0;
@@ -44,6 +64,13 @@ angular.module('harmonogram', ['ngRoute'])
     for(var i = 0; i < $scope.zaklady.length; ++i) {
       if($scope.zaklady[i].z == index) {
         return $scope.zaklady[i].title;
+      }
+    }
+  }
+  $scope.wstawPlan = function(x, y, zaklad) {
+    for(var i = 0; i < $scope.plany.length; ++i){
+      if($scope.plany[i].x == x && $scope.plany[i].y == y && $scope.plany[i].z == zaklad){
+        return $scope.plany[i].content;
       }
     }
   }
